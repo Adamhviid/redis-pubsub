d34e7cfeea804083fd1f6fee83a8f4180da42c64
Adam <hviidadam@gmail.com>
2023-05-27 13:22:36 +0200

    redis pub-sub example

---
+++ script.js
@@ -0,0 +1,36 @@
+const publishButton = document.getElementById("publishButton");
+const messageInput = document.getElementById("messageInput");
+const messageList = document.getElementById("messageList");
+
+const fetchMessages = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/subscribe");
+    const data = await response.json();
+
+    if (response.ok) {
+      messageList.innerHTML = data.messages;
+    } else {
+      console.error("Failed to fetch messages:", data.error);
+    }
+  } catch (error) {
+    console.error("Failed to fetch messages:", error);
+  }
+};
+
+publishButton.addEventListener("click", async () => {
+  const message = messageInput.value;
+
+  try {
+    const publishResponse = await fetch('http://localhost:3000/publish', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: message }),
+    });
+  } catch (error) {
+    console.error("Failed to publish message:", error);
+  }
+});
+
+fetchMessages();
---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import ioredis from 'ioredis';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const redisSubscriber = new ioredis(6379, 'localhost');
+const redisPublisher = new ioredis(6379, 'localhost');
+
+app.post('/publish', async (req, res) => {
+  const { message } = req.body;
+  console.log(message);
+
+  await redisPublisher.publish('channel', message, (err) => {
+    if (err) {
+      console.error('Error publishing message:', err);
+      res.status(500).json({ error: 'Failed to publish message' });
+    } else {
+      console.log(`Published: ${message}`);
+      res.status(200).json({ message: message });
+    }
+  });
+});
+
+app.get('/subscribe', async (req, res) => {
+  await redisSubscriber.subscribe('channel', (err) => {
+    if (err) {
+      console.error('Error subscribing to channel:', err);
+      res.status(500).json({ error: 'Failed to subscribe to channel' });
+    } else {
+      console.log('Redis subscriber connected');
+    }
+  });
+
+  // Listen for messages
+  redisSubscriber.on('message', (channel, message) => {
+    console.log(`Received message: ${message} from ${channel}`);
+    res.status(200).json({ messages: [message] });
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
